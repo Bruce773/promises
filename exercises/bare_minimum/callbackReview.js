22ba367bbf52acb83e405d534960da7f907787c6
Bruce <bruce@yogainternational.com>
2019-03-14 23:17:29 -0400

    Complete bare_minimum/callbackReview.js exercises

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,17 +6,41 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function(filePath, callback) {
   // TODO
+  fs.readFile(`${filePath}`, (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      var string = [];
+      for (var i = 0; i < result.length; i++) {
+        if (result.toString()[i] !== '\n') {
+          string.push(result.toString()[i]);
+        } else {
+          break;
+        }
+      }
+      // console.log(string.join(''));
+      callback(null, string.join(''));
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function(url, callback) {
   // TODO
+  request(url, 'GET', (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      // console.log(result);
+      callback(null, result.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
   getStatusCode: getStatusCode,
-  pluckFirstLineFromFile: pluckFirstLineFromFile
+  pluckFirstLineFromFile: pluckFirstLineFromFile,
 };
